d4e595ce29d29c39cd16ecd999bf71fbcf0de1a6
dnabr04 <diegomatnf2004@gmail.com>
2025-03-22 14:53:38 +0100

    Insert the login activity and create the activity layout.

--- app/src/main/java/com/example/grinnet/LoginActivity.kt
+++ app/src/main/java/com/example/grinnet/LoginActivity.kt
@@ -7,18 +7,16 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
