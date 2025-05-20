b2f5f6b518d162d219123c31aa09493087932144
dnabr04 <diegomatnf2004@gmail.com>
2025-05-20 21:08:17 +0200

    Save image from gallery on firebase storage

--- app/src/main/java/com/example/grinnet/CreatePostActivity.kt
+++ app/src/main/java/com/example/grinnet/CreatePostActivity.kt
@@ -1,11 +1,18 @@
 package com.example.grinnet
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,6 +23,7 @@ import com.example.grinnet.data.UserRequest
 import com.example.grinnet.utils.SessionManager
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import retrofit2.Call
 import retrofit2.Callback
@@ -23,13 +31,15 @@ import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.UUID
 
 class CreatePostActivity : AppCompatActivity() {
 
     private var postRelatedValue: Long? = null
-//    private val storage = Firebase.storage(getString(R.string.BucketURL))
+    private lateinit var storage: FirebaseStorage
     private val auth = Firebase.auth
     private val firebaseId = auth.currentUser!!.uid
+    private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,9 +57,13 @@ class CreatePostActivity : AppCompatActivity() {
             postRelatedValue = extras.getLong("postRelated")
         }
 
+        storage = Firebase.storage(getString(R.string.BucketURL))
+
+        setGalleryLauncher()
+
         val publishButton = findViewById<Button>(R.id.publishButton)
         val closeButton = findViewById<Button>(R.id.closeButton)
-        val addImageButton = findViewById<Button>(R.id.addImage)
+        val addImageButton = findViewById<ImageButton>(R.id.addImage)
 
         publishButton.setOnClickListener {
             createPost()
@@ -60,10 +74,39 @@ class CreatePostActivity : AppCompatActivity() {
         }
 
         addImageButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-                type = "image/* video/*"
+            openGallery()
+        }
+    }
+
+    private fun setGalleryLauncher() {
+        pickImageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val imageUri: Uri? = data?.data
+                if (imageUri != null) {
+                    val ref = storage.reference
+                    val imageRef = ref.child("images/$firebaseId/${UUID.randomUUID()}.jpg")
+                    val uploadTask = imageRef.putFile(imageUri)
+
+                    uploadTask.addOnSuccessListener {
+                        imageRef.downloadUrl.addOnCompleteListener {
+                            uri ->
+                            val downloadUrl = uri.toString()
+                            Toast.makeText(this, "Imagen subida", Toast.LENGTH_SHORT).show()
+                        }
+                    }.addOnFailureListener {
+                        Toast.makeText(this, "Error al subir la imagen", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/* video/*"
+        }
+        pickImageLauncher.launch(intent)
     }
 
     private fun createPost() {
